4a119d37e71ebff4756c54871c3719a16150cd43
Jorge Andrés Rodarte Martínez <jrodarte@ciatec.int>
2017-09-12 12:26:52 -0500

    Proyecto finalizado para calificar v0.2

--- 03/express/routes/index.js
+++ 03/express/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/indice', function (req, res, next) {
-  /*res.json({ 
+router.use('/', function (req, res, next) {
+  res.json({ 
     title: 'Express',
     time: new Date() - req.startedAt 
-  });*/ 
-  var err = new Error('No encontrado Not Found');
-  err.status = 404;
-  next(err);
+  }); 
+  
 });
 
 router.use('/', (err, req, res, next) => {
